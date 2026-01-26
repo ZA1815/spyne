bafc471f30e0df0919e6d2aeeb16ed914fc8ae13
Zaid Ahmed <zaahme18@gmail.com>
2026-01-26 13:29:02 -0500

    chore(serde): Pause working on writing SerDe tests

--- spyne/tests/serialization.rs
+++ spyne/tests/serialization.rs
@@ -1,6 +1,7 @@
 use spyne::primitives::serialization::BinarySerde;
 use spyne::macros::serialization::{Deserialize, Serialize};
 
+// Add pub to struct to test later
 #[derive(Debug, Clone, PartialEq, Eq, Deserialize, Serialize)]
 struct TestStruct {
     a: Vec<u32>,
