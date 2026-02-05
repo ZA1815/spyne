27ac37cf3e3392ceeefda6c75830d4aaf32492a8
ZA1815 <zaahme18@gmail.com>
2026-02-04 19:32:11 -0500

    feat(metal): Create ObjCFunctionPointers for use in Metal

---
+++ spyne-ffi/src/c/macos/metal/objc_runtime.rs
@@ -0,0 +1,51 @@
+use std::{ffi::{CString, c_char, c_void}, mem::transmute};
+
+use crate::c::{linux::general::constants::RTLD_NOW, macos::general::functions::{dlopen, dlsym}};
+
+pub type Id = *mut c_void;
+
+pub type ObjCGetClass = unsafe extern "C" fn(
+    name: *const c_char
+) -> Id;
+
+pub type Sel = *mut c_void;
+
+pub type SelRegisterName = unsafe extern "C" fn(
+    str: *const c_char
+) -> Sel;
+
+pub type ObjCMsgSend = unsafe extern "C" fn();
+
+pub struct ObjCFunctionPointers {
+    pub objc_get_class: ObjCGetClass,
+    pub sel_register_name: SelRegisterName,
+    pub objc_msg_send: ObjCMsgSend
+}
+
+impl ObjCFunctionPointers {
+    pub unsafe fn load() -> Self {
+        let lib = unsafe { dlopen(CString::new("libobjc.dylib").unwrap().as_ptr(), RTLD_NOW) };
+        let objc_get_class: ObjCGetClass = unsafe { transmute(dlsym(lib, CString::new("objc_getClass").unwrap().as_ptr())) };
+        let sel_register_name: SelRegisterName = unsafe { transmute(dlsym(lib, CString::new("sel_registerName").unwrap().as_ptr())) };
+        let objc_msg_send: ObjCMsgSend = unsafe { transmute(dlsym(lib, CString::new("objc_msgSend").unwrap().as_ptr())) };
+        Self {
+            objc_get_class,
+            sel_register_name,
+            objc_msg_send
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use std::ffi::CString;
+
+    use crate::c::macos::metal::objc_runtime::ObjCFunctionPointers;
+
+    #[test]
+    fn test_objc_funcs() {
+        let funcs = unsafe { ObjCFunctionPointers::load() };
+        let cls = unsafe { (funcs.objc_get_class)(CString::new("NSObject").unwrap().as_ptr()) };
+        assert!(!cls.is_null())
+    }
+}
--- spyne-ffi/src/c/macos/mod.rs
+++ spyne-ffi/src/c/macos/mod.rs
@@ -2,4 +2,6 @@ pub mod general;
 
 pub mod kqueue;
 
+pub mod metal;
+
 pub mod pty;
