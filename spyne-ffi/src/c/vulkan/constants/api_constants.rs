9576e66bd361f969c2343c9759be9913409a8873
ZA1815 <zaahme18@gmail.com>
2026-02-07 14:22:25 -0500

    feat(vulkan): Add VK_MAX_SIZE as an API constant

--- spyne-ffi/src/c/vulkan/constants/api_constants.rs
+++ spyne-ffi/src/c/vulkan/constants/api_constants.rs
@@ -2,3 +2,4 @@ pub const VK_MAX_PHYSICAL_DEVICE_NAME_SIZE: usize = 256;
 pub const VK_UUID_SIZE: usize = 16;
 pub const VK_MAX_MEMORY_TYPES: usize = 32;
 pub const VK_MAX_MEMORY_HEAPS: usize = 16;
+pub const VK_MAX_SIZE: usize = usize::MAX;
