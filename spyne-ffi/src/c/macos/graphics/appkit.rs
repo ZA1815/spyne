2785f30af3572d683c24f8ee280a170f13894cb9
ZA1815 <zaahme18@gmail.com>
2026-02-08 13:55:57 -0500

    fix(appkit): Remove accidental namespacing of constants

--- spyne-ffi/src/c/macos/graphics/appkit.rs
+++ spyne-ffi/src/c/macos/graphics/appkit.rs
@@ -18,21 +18,19 @@ impl BitOr for NSWindowStyleMask {
         Self(self.0 | rhs.0)
     }
 }
-impl NSWindowStyleMask {
-    pub const NS_WINDOW_STYLE_MASK_BORDERLESS: Self = Self(0);
-    pub const NS_WINDOW_STYLE_MASK_TITLED: Self = Self(1 << 0);
-    pub const NS_WINDOW_STYLE_MASK_CLOSABLE: Self = Self(1 << 1);
-    pub const NS_WINDOW_STYLE_MASK_MINIATURIZABLE: Self = Self(1 << 2);
-    pub const NS_WINDOW_STYLE_MASK_RESIZABLE: Self = Self(1 << 3);
-    pub const NS_WINDOW_STYLE_MASK_UTILITY_WINDOW: Self = Self(1 << 4);
-    pub const NS_WINDOW_STYLE_MASK_DOC_MODAL_WINDOW: Self = Self(1 << 6);
-    pub const NS_WINDOW_STYLE_MASK_NONACTIVATING_PANEL: Self = Self(1 << 7);
-    pub const NS_WINDOW_STYLE_MASK_TEXTURED_BACKGROUND: Self = Self(1 << 8);
-    pub const NS_WINDOW_STYLE_MASK_UNIFIED_TITLE_AND_TOOLBAR: Self = Self(1 << 12);
-    pub const NS_WINDOW_STYLE_MASK_HUD_WINDOW: Self = Self(1 << 13);
-    pub const NS_WINDOW_STYLE_MASK_FULL_SCREEN: Self = Self(1 << 14);
-    pub const NS_WINDOW_STYLE_MASK_FULL_SIZE_CONTENT_VIEW: Self = Self(1 << 15);
-}
+pub const NS_WINDOW_STYLE_MASK_BORDERLESS: NSWindowStyleMask = NSWindowStyleMask(0);
+pub const NS_WINDOW_STYLE_MASK_TITLED: NSWindowStyleMask = NSWindowStyleMask(1 << 0);
+pub const NS_WINDOW_STYLE_MASK_CLOSABLE: NSWindowStyleMask = NSWindowStyleMask(1 << 1);
+pub const NS_WINDOW_STYLE_MASK_MINIATURIZABLE: NSWindowStyleMask = NSWindowStyleMask(1 << 2);
+pub const NS_WINDOW_STYLE_MASK_RESIZABLE: NSWindowStyleMask = NSWindowStyleMask(1 << 3);
+pub const NS_WINDOW_STYLE_MASK_UTILITY_WINDOW: NSWindowStyleMask = NSWindowStyleMask(1 << 4);
+pub const NS_WINDOW_STYLE_MASK_DOC_MODAL_WINDOW: NSWindowStyleMask = NSWindowStyleMask(1 << 6);
+pub const NS_WINDOW_STYLE_MASK_NONACTIVATING_PANEL: NSWindowStyleMask = NSWindowStyleMask(1 << 7);
+pub const NS_WINDOW_STYLE_MASK_TEXTURED_BACKGROUND: NSWindowStyleMask = NSWindowStyleMask(1 << 8);
+pub const NS_WINDOW_STYLE_MASK_UNIFIED_TITLE_AND_TOOLBAR: NSWindowStyleMask = NSWindowStyleMask(1 << 12);
+pub const NS_WINDOW_STYLE_MASK_HUD_WINDOW: NSWindowStyleMask = NSWindowStyleMask(1 << 13);
+pub const NS_WINDOW_STYLE_MASK_FULL_SCREEN: NSWindowStyleMask = NSWindowStyleMask(1 << 14);
+pub const NS_WINDOW_STYLE_MASK_FULL_SIZE_CONTENT_VIEW: NSWindowStyleMask = NSWindowStyleMask(1 << 15);
 
 pub struct NSBackingStoreType(usize);
 impl BitOr for NSBackingStoreType {
@@ -42,6 +40,4 @@ impl BitOr for NSBackingStoreType {
         Self(self.0 | rhs.0)
     }
 }
-impl NSBackingStoreType {
-    pub const NS_BACKING_STORE_BUFFERED: Self = Self(1 << 1);
-}
+pub const NS_BACKING_STORE_BUFFERED: NSBackingStoreType = NSBackingStoreType(1 << 1);
