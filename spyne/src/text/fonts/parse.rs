c55f55b5af5931d68808322ac0a6188395dfbaf5
Zaid Ahmed <zaahme18@gmail.com>
2026-02-11 23:01:55 -0500

    feat(fonts): Finish parsing cmap table

--- spyne/src/text/fonts/parse.rs
+++ spyne/src/text/fonts/parse.rs
@@ -158,25 +158,180 @@ impl FontFile {
         }
     }
     
+    pub fn parse_cmap(&self) -> Result<CmapTable, Error> {
+        let cmap_bytes = self.get_table(b"cmap")?;
+        
+        let version = u16::from_be_bytes(cmap_bytes.get(0..2).ok_or(ErrorKind::UnexpectedEof)?.try_into().unwrap());
+        if version != 0 {
+            return Err(Error::new(ErrorKind::InvalidData, "Version number isn't zero"));
+        }
+        let num_tables = u16::from_be_bytes(cmap_bytes.get(2..4).ok_or(ErrorKind::UnexpectedEof)?.try_into().unwrap());
+        let mut encoding_records: Vec<EncodingRecord> = Vec::new();
+        let mut count = 4;
+        for _ in 0..num_tables {
+            let platform_id = u16::from_be_bytes(cmap_bytes.get(count..count + 2).ok_or(ErrorKind::UnexpectedEof)?.try_into().unwrap());
+            let encoding_id = u16::from_be_bytes(cmap_bytes.get(count + 2..count + 4).ok_or(ErrorKind::UnexpectedEof)?.try_into().unwrap());
+            let offset = u32::from_be_bytes(cmap_bytes.get(count + 4..count + 8).ok_or(ErrorKind::UnexpectedEof)?.try_into().unwrap());
+            encoding_records.push(EncodingRecord { platform_id, encoding_id, offset });
+            count += 8;
+        }
+        
+        let mut subtables: Vec<CmapSubtable> = Vec::new();
+        for rec in encoding_records.iter() {
+            let mut offset = rec.offset as usize;
+            let format = u16::from_be_bytes(cmap_bytes.get(offset..offset + 2).ok_or(ErrorKind::UnexpectedEof)?.try_into().unwrap());
+            offset += 2;
+            match format {
+                0 => {
+                    let length = u16::from_be_bytes(cmap_bytes.get(offset..offset + 2).ok_or(ErrorKind::UnexpectedEof)?.try_into().unwrap());
+                    let language = u16::from_be_bytes(cmap_bytes.get(offset + 2..offset + 4).ok_or(ErrorKind::UnexpectedEof)?.try_into().unwrap());
+                    let glyph_id_array: [u8; 256] = cmap_bytes.get(offset + 4..offset + 260).ok_or(ErrorKind::UnexpectedEof)?.try_into().unwrap();
+                    
+                    subtables.push(CmapSubtable::Format0 { length, language, glyph_id_array });
+                }
+                2 => {
+                    let length = u16::from_be_bytes(cmap_bytes.get(offset..offset + 2).ok_or(ErrorKind::UnexpectedEof)?.try_into().unwrap());
+                    let language = u16::from_be_bytes(cmap_bytes.get(offset + 2..offset + 4).ok_or(ErrorKind::UnexpectedEof)?.try_into().unwrap());
+                    let sub_header_keys: [u16; 256] = cmap_bytes.get(offset + 4..offset + 516).ok_or(ErrorKind::UnexpectedEof)?.chunks_exact(2).map(|ch| {
+                        u16::from_be_bytes(ch.try_into().unwrap())
+                    }).collect::<Vec<u16>>().try_into().unwrap();
+                    offset += 516;
+                    let sub_headers_num = (sub_header_keys.iter().max().unwrap() / 8) + 1;
+                    let sub_headers: Vec<SubHeader> = cmap_bytes.get(offset..offset + sub_headers_num as usize * 8).ok_or(ErrorKind::UnexpectedEof)?.chunks_exact(8).map(|ch| {
+                        let first_code = u16::from_be_bytes(ch[0..2].try_into().unwrap());
+                        let entry_count = u16::from_be_bytes(ch[2..4].try_into().unwrap());
+                        let id_delta = i16::from_be_bytes(ch[4..6].try_into().unwrap());
+                        let id_range_offset = u16::from_be_bytes(ch[6..8].try_into().unwrap());
+                        
+                        SubHeader { first_code, entry_count, id_delta, id_range_offset }
+                    }).collect();
+                    offset += sub_headers_num as usize * 8;
+                    let glyph_id_array: Vec<u16> = cmap_bytes.get(offset..).ok_or(ErrorKind::UnexpectedEof)?.chunks_exact(2).map(|ch| {
+                        u16::from_be_bytes(ch.try_into().unwrap())
+                    }).collect();
+                    
+                    subtables.push(CmapSubtable::Format2 { length, language, sub_header_keys, sub_headers, glyph_id_array });
+                }
+                4 => {
+                    let length = u16::from_be_bytes(cmap_bytes.get(offset..offset + 2).ok_or(ErrorKind::UnexpectedEof)?.try_into().unwrap());
+                    let language = u16::from_be_bytes(cmap_bytes.get(offset + 2..offset + 4).ok_or(ErrorKind::UnexpectedEof)?.try_into().unwrap());
+                    let seg_count_x2 = u16::from_be_bytes(cmap_bytes.get(offset + 4..offset + 6).ok_or(ErrorKind::UnexpectedEof)?.try_into().unwrap());
+                    let search_range = u16::from_be_bytes(cmap_bytes.get(offset + 6..offset + 8).ok_or(ErrorKind::UnexpectedEof)?.try_into().unwrap());
+                    let entry_selector = u16::from_be_bytes(cmap_bytes.get(offset + 8..offset + 10).ok_or(ErrorKind::UnexpectedEof)?.try_into().unwrap());
+                    let range_shift = u16::from_be_bytes(cmap_bytes.get(offset + 10..offset + 12).ok_or(ErrorKind::UnexpectedEof)?.try_into().unwrap());
+                    offset += 12;
+                    let end_code: Vec<u16> = cmap_bytes.get(offset..offset + seg_count_x2 as usize).ok_or(ErrorKind::UnexpectedEof)?.chunks_exact(2).map(|ch| {
+                        u16::from_be_bytes(ch.try_into().unwrap())
+                    }).collect();
+                    offset += seg_count_x2 as usize;
+                    let _reserved_pad = u16::from_be_bytes(cmap_bytes.get(offset..offset + 2).ok_or(ErrorKind::UnexpectedEof)?.try_into().unwrap());
+                    offset += 2;
+                    let start_code: Vec<u16> = cmap_bytes.get(offset..offset + seg_count_x2 as usize).ok_or(ErrorKind::UnexpectedEof)?.chunks_exact(2).map(|ch| {
+                        u16::from_be_bytes(ch.try_into().unwrap())
+                    }).collect();
+                    offset += seg_count_x2 as usize / 2;
+                    let id_delta: Vec<i16> = cmap_bytes.get(offset..offset + seg_count_x2 as usize).ok_or(ErrorKind::UnexpectedEof)?.chunks_exact(2).map(|ch| {
+                        i16::from_be_bytes(ch.try_into().unwrap())
+                    }).collect();
+                    offset += seg_count_x2 as usize / 2;
+                    let id_range_offset: Vec<u16> = cmap_bytes.get(offset..offset + seg_count_x2 as usize).ok_or(ErrorKind::UnexpectedEof)?.chunks_exact(2).map(|ch| {
+                        u16::from_be_bytes(ch.try_into().unwrap())
+                    }).collect();
+                    offset += seg_count_x2 as usize / 2;
+                    let glyph_id_array: Vec<u16> = cmap_bytes.get(offset..).ok_or(ErrorKind::UnexpectedEof)?.chunks_exact(2).map(|ch| {
+                        u16::from_be_bytes(ch.try_into().unwrap())
+                    }).collect();
+                    
+                    subtables.push(CmapSubtable::Format4 { length, language, seg_count_x2, search_range, entry_selector, range_shift, end_code, _reserved_pad, start_code, id_delta, id_range_offset, glyph_id_array });
+                }
+                6 => {
+                    let length = u16::from_be_bytes(cmap_bytes.get(offset..offset + 2).ok_or(ErrorKind::UnexpectedEof)?.try_into().unwrap());
+                    let language = u16::from_be_bytes(cmap_bytes.get(offset + 2..offset + 4).ok_or(ErrorKind::UnexpectedEof)?.try_into().unwrap());
+                    let first_code = u16::from_be_bytes(cmap_bytes.get(offset + 4..offset + 6).ok_or(ErrorKind::UnexpectedEof)?.try_into().unwrap());
+                    let entry_count = u16::from_be_bytes(cmap_bytes.get(offset + 6..offset + 8).ok_or(ErrorKind::UnexpectedEof)?.try_into().unwrap());
+                    offset += 8;
+                    let glyph_id_array: Vec<u16> = cmap_bytes.get(offset..offset + entry_count as usize * 2).ok_or(ErrorKind::UnexpectedEof)?.chunks_exact(2).map(|ch| {
+                        u16::from_be_bytes(ch.try_into().unwrap())
+                    }).collect();
+                    
+                    subtables.push(CmapSubtable::Format6 { length, language, first_code, entry_count, glyph_id_array });
+                }
+                12 => {
+                    let _reserved = u16::from_be_bytes(cmap_bytes.get(offset..offset + 2).ok_or(ErrorKind::UnexpectedEof)?.try_into().unwrap());
+                    let length = u32::from_be_bytes(cmap_bytes.get(offset + 2..offset + 6).ok_or(ErrorKind::UnexpectedEof)?.try_into().unwrap());
+                    let language = u32::from_be_bytes(cmap_bytes.get(offset + 6..offset + 10).ok_or(ErrorKind::UnexpectedEof)?.try_into().unwrap());
+                    let num_groups = u32::from_be_bytes(cmap_bytes.get(offset + 10..offset + 14).ok_or(ErrorKind::UnexpectedEof)?.try_into().unwrap());
+                    let groups: Vec<Group> = cmap_bytes.get(offset + 14..).ok_or(ErrorKind::UnexpectedEof)?.chunks_exact(num_groups as usize).map(|ch| {
+                        let start_char_code = u32::from_be_bytes(ch[0..4].try_into().unwrap());
+                        let end_char_code = u32::from_be_bytes(ch[4..8].try_into().unwrap());
+                        let start_glyph_id = u32::from_be_bytes(ch[8..].try_into().unwrap());
+                        
+                        Group { start_char_code, end_char_code, start_glyph_id }
+                    }).collect();
+                    
+                    subtables.push(CmapSubtable::Format12 { _reserved, length, language, num_groups, groups });
+                }
+                13 => {
+                    let _reserved = u16::from_be_bytes(cmap_bytes.get(offset..offset + 2).ok_or(ErrorKind::UnexpectedEof)?.try_into().unwrap());
+                    let length = u32::from_be_bytes(cmap_bytes.get(offset + 2..offset + 6).ok_or(ErrorKind::UnexpectedEof)?.try_into().unwrap());
+                    let language = u32::from_be_bytes(cmap_bytes.get(offset + 6..offset + 10).ok_or(ErrorKind::UnexpectedEof)?.try_into().unwrap());
+                    let num_groups = u32::from_be_bytes(cmap_bytes.get(offset + 10..offset + 14).ok_or(ErrorKind::UnexpectedEof)?.try_into().unwrap());
+                    let groups: Vec<Group> = cmap_bytes.get(offset + 14..).ok_or(ErrorKind::UnexpectedEof)?.chunks_exact(num_groups as usize).map(|ch| {
+                        let start_char_code = u32::from_be_bytes(ch[0..4].try_into().unwrap());
+                        let end_char_code = u32::from_be_bytes(ch[4..8].try_into().unwrap());
+                        let start_glyph_id = u32::from_be_bytes(ch[8..].try_into().unwrap());
+                        
+                        Group { start_char_code, end_char_code, start_glyph_id }
+                    }).collect();
+                    
+                    subtables.push(CmapSubtable::Format13 { _reserved, length, language, num_groups, groups });
+                }
+                14 => {
+                    let length = u32::from_be_bytes(cmap_bytes.get(offset..offset + 4).ok_or(ErrorKind::UnexpectedEof)?.try_into().unwrap());
+                    let num_var_selector_records = u32::from_be_bytes(cmap_bytes.get(offset + 4..offset + 8).ok_or(ErrorKind::UnexpectedEof)?.try_into().unwrap());
+                    let var_selector: Vec<VariationSelectorRecord> = cmap_bytes.get(offset + 8..).ok_or(ErrorKind::UnexpectedEof)?.chunks_exact(num_var_selector_records as usize).map(|ch| {
+                        let var_selector: [u8; 3] = ch[0..3].try_into().unwrap();
+                        let default_uvs_offset = u32::from_be_bytes(ch[3..7].try_into().unwrap());
+                        let non_default_uvs_offset = u32::from_be_bytes(ch[7..11].try_into().unwrap());
+                        
+                        VariationSelectorRecord { var_selector, default_uvs_offset, non_default_uvs_offset }
+                    }).collect();
+                    
+                    subtables.push(CmapSubtable::Format14 { length, num_var_selector_records, var_selector });
+                }
+                8 | 10 => return Err(Error::new(ErrorKind::InvalidData, "Subtable formats 8 and 10 are considered deprecated")),
+                _ => return Err(Error::new(ErrorKind::InvalidData, ""))
+            }
+        }
+        
+        Ok(CmapTable { version, num_tables, encoding_records, subtables })
+    }
+    
     pub fn parse_loca(&self, num_glyphs: u16, index_to_loc_format: i16) -> Result<Vec<u32>, Error> {
         let loca_bytes = self.get_table(b"loca")?;
         
         let mut indices: Vec<u32> = Vec::new();
         match index_to_loc_format {
             0 => {
-                for i in 0..num_glyphs + 1 {
-                    indices.push(loca_bytes[i as usize + 1] - loca_bytes[i as usize]);
+                for i in (0..(num_glyphs as usize + 1) * 2).step_by(2) {
+                    let offset = u16::from_be_bytes(loca_bytes.get(i..i + 2).ok_or(ErrorKind::UnexpectedEof)?.try_into().unwrap()); 
+                    indices.push((offset * 2) as u32);
                 }
             },
             1 => {
-                for i in 0..num_glyphs + 1 {
-                    
+                for i in (0..(num_glyphs as usize + 1) * 4).step_by(4) {
+                    let offset = u32::from_be_bytes(loca_bytes.get(i..i + 4).ok_or(ErrorKind::UnexpectedEof)?.try_into().unwrap());
+                    indices.push(offset);
                 }
             },
             _ => return Err(Error::new(ErrorKind::InvalidInput, "index_to_loc_format was not 0 or 1"))
         }
         
         Ok(indices)
+    }
+    
+    pub fn parse_glyf(&self, loca_indices: Vec<u32>) {
+        
     }
 }
 
@@ -223,4 +378,95 @@ struct MaxpTable {
     pub max_size_of_instructions: Option<u16>,
     pub max_components_elements: Option<u16>,
     pub max_component_depth: Option<u16>
+}
+
+struct CmapTable {
+    pub version: u16,
+    pub num_tables: u16,
+    pub encoding_records: Vec<EncodingRecord>,
+    pub subtables: Vec<CmapSubtable>
+}
+
+struct EncodingRecord {
+    pub platform_id: u16,
+    pub encoding_id: u16,
+    pub offset: u32
+}
+
+pub enum CmapSubtable {
+    Format0 {
+        length: u16,
+        language: u16,
+        glyph_id_array: [u8; 256]
+    },
+    Format2 {
+        length: u16,
+        language: u16,
+        sub_header_keys: [u16; 256],
+        sub_headers: Vec<SubHeader>,
+        glyph_id_array: Vec<u16>
+    },
+    Format4 {
+        length: u16,
+        language: u16,
+        seg_count_x2: u16,
+        search_range: u16,
+        entry_selector: u16,
+        range_shift: u16,
+        end_code: Vec<u16>,
+        _reserved_pad: u16,
+        start_code: Vec<u16>,
+        id_delta: Vec<i16>,
+        id_range_offset: Vec<u16>,
+        glyph_id_array: Vec<u16>
+    },
+    Format6 {
+        length: u16,
+        language: u16,
+        first_code: u16,
+        entry_count: u16,
+        glyph_id_array: Vec<u16>
+    },
+    Format12 {
+        _reserved: u16,
+        length: u32,
+        language: u32,
+        num_groups: u32,
+        groups: Vec<Group>
+    },
+    Format13 {
+        _reserved: u16,
+        length: u32,
+        language: u32,
+        num_groups: u32,
+        groups: Vec<Group>
+    },
+    Format14 {
+        length: u32,
+        num_var_selector_records: u32,
+        var_selector: Vec<VariationSelectorRecord>
+    }
+}
+
+struct SubHeader {
+    pub first_code: u16,
+    pub entry_count: u16,
+    pub id_delta: i16,
+    pub id_range_offset: u16
+}
+
+struct Group {
+    pub start_char_code: u32,
+    pub end_char_code: u32,
+    pub start_glyph_id: u32
+}
+
+struct VariationSelectorRecord {
+    pub var_selector: [u8; 3],
+    pub default_uvs_offset: u32,
+    pub non_default_uvs_offset: u32
+}
+
+struct GlyfTable {
+    
 }
