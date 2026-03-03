0ad53169f246e6cd06b48020db773d93184e24ed
Zaid Ahmed <zaahme18@gmail.com>
2026-03-02 20:00:12 -0500

    feat(http): Finish implementing the send function, which is all I need from HTTP for now

--- spyne-net/src/http/h11/send.rs
+++ spyne-net/src/http/h11/send.rs
@@ -1,5 +1,197 @@
-use crate::http::h11::structures::{Request, Response};
+use std::{collections::HashMap, io::{ErrorKind, Read, Write}, net::{TcpStream, ToSocketAddrs}, time::Duration};
 
-pub fn send(request: Request) -> Response {
+use crate::http::h11::structures::{HttpError, Request, Response, StatusLine, Version};
+
+pub fn send<A>(addr: A, request: Request) -> Result<Response, HttpError>
+where A: ToSocketAddrs {
+    let mut connection = TcpStream::connect(addr).map_err(|e| HttpError::TcpFailure(e))?;
     
+    let request_bytes = serialize_request(request);
+    connection.write_all(&request_bytes).map_err(|e| HttpError::WriteFailure(e))?;
+    
+    Ok(read_response(&mut connection)?)
+}
+
+fn serialize_request(request: Request) -> Vec<u8> {
+    let mut req_vec: Vec<u8> = Vec::new();
+    
+    req_vec.extend(request.request_line.method.as_bytes());
+    req_vec.push(b' ');
+    req_vec.extend(request.request_line.path.as_bytes());
+    req_vec.push(b' ');
+    req_vec.extend(request.request_line.version.as_bytes());
+    req_vec.extend(b"\r\n");
+    
+    for (key, value) in request.header {
+        req_vec.extend(format!("{}:{}\r\n", key, value).as_bytes());
+    }
+    req_vec.extend(b"\r\n");
+    
+    if let Some(body) = request.body {
+        req_vec.extend(body);
+    }
+    
+    req_vec
+}
+
+fn read_response(connection: &mut TcpStream) -> Result<Response, HttpError> {
+    let mut response: Vec<u8> = Vec::new();
+    let mut prev_len: usize = 0;
+    connection.set_read_timeout(Some(Duration::new(10, 0))).map_err(|e| HttpError::TimeoutFailure(e))?;
+    let header_end_pos = loop {
+        let mut chunk: [u8; 4096] = [0; 4096];
+        let bytes_read = connection.read(&mut chunk).map_err(|e| {
+            match e.kind() {
+                ErrorKind::TimedOut | ErrorKind::WouldBlock => HttpError::ReadTimeout(e),
+                _ => HttpError::ReadFailure(e)
+            }
+        })?;
+        response.reserve(bytes_read);
+        response.extend_from_slice(&chunk[0..bytes_read]);
+        let header_end = response[prev_len.saturating_sub(3)..].windows(4).position(|ch| ch == b"\r\n\r\n");
+        if let Some(pos) = header_end {
+            break pos;
+        }
+        else {
+            prev_len += bytes_read;
+        }
+    } + prev_len.saturating_sub(3);
+    let (status_line, header) = deserialize_top(&response[0..header_end_pos])?;
+    
+    match status_line.status {
+        100..=199 | 204 | 205 | 304 => return Ok(Response { status_line, header, body: None }),
+        _ => ()
+    }
+    
+    let mut body: Vec<u8>;
+    if let Some(te) = header.get("Transfer-Encoding") {
+        if te != "chunked" {
+            return Err(HttpError::InvalidHeader);
+        }
+        body = Vec::new();
+        let mut temp_buf: Vec<u8> = response[header_end_pos + 4..].to_vec();
+        let mut cursor = 0;
+        loop {
+            if cursor > 0xFFFFFFFF {
+                temp_buf.drain(0..cursor);
+                cursor = 0;
+            }
+            
+            let mut chunk: [u8; 4096] = [0; 4096];
+            let mut prev_len: usize = cursor + 1;
+            let digit_data_pos = loop {
+                let bytes_read = connection.read(&mut chunk).map_err(|e| {
+                    match e.kind() {
+                        ErrorKind::TimedOut | ErrorKind::WouldBlock => HttpError::ReadTimeout(e),
+                        _ => HttpError::ReadFailure(e)
+                    }
+                })?;
+                temp_buf.extend_from_slice(&chunk[0..bytes_read]);
+                let digit_split = temp_buf[prev_len.saturating_sub(1)..].windows(2).position(|ch| ch == b"\r\n");
+                if let Some(pos) = digit_split {
+                    break pos;
+                }
+                else {
+                    prev_len += bytes_read;
+                }
+            } + prev_len.saturating_sub(1);
+            
+            let ch_len_bytes = str::from_utf8(&temp_buf[cursor..digit_data_pos])
+                .map_err(|e| HttpError::InvalidPayload(e))?;
+            let ch_len = usize::from_str_radix(ch_len_bytes, 16)
+                .map_err(|e| HttpError::InvalidChunkSize(e))?;
+            if ch_len == 0 {
+                break;
+            }
+            else if ch_len > 0xFFFFFFFF {
+                return Err(HttpError::ChunkSizeLimitExceeded);
+            }
+            let ch_data = &temp_buf[digit_data_pos + 2..];
+            if ch_data.len() > ch_len {
+                body.reserve(ch_len);
+                body.extend_from_slice(&ch_data[..ch_len]);
+                cursor += ch_len_bytes.len() + ch_len + 4;
+            }
+            else {
+                body.reserve(ch_data.len());
+                body.extend_from_slice(ch_data);
+                cursor += ch_len_bytes.len() + ch_data.len() + 2;
+                
+                let mut chunk: Vec<u8> = vec![0; ch_len - ch_data.len() + 2];
+                connection.read_exact(&mut chunk).map_err(|e| {
+                    match e.kind() {
+                        ErrorKind::TimedOut | ErrorKind::WouldBlock => HttpError::ReadTimeout(e),
+                        _ => HttpError::ReadFailure(e)
+                    }
+                })?;
+                body.reserve(chunk.len() - 2);
+                body.extend(&chunk[..chunk.len() - 2]);
+            }
+        }
+    }
+    else if let Some(cl) = header.get("Content-Length") {
+        let content_length = cl.parse::<usize>().map_err(|e| HttpError::InvalidContentLength(e))?;
+        body = Vec::with_capacity(content_length);
+        body.extend_from_slice(&response[header_end_pos + 4..]);
+        let mut body_byte_num: usize = body.len();
+        while content_length > body_byte_num {
+            let mut chunk: [u8; 4096] = [0; 4096];
+            let bytes_read = connection.read(&mut chunk).map_err(|e| {
+                match e.kind() {
+                    ErrorKind::TimedOut | ErrorKind::WouldBlock => HttpError::ReadTimeout(e),
+                    _ => HttpError::ReadFailure(e)
+                }
+            })?;
+            body.reserve(bytes_read);
+            body.extend_from_slice(&chunk[0..bytes_read]);
+            body_byte_num += bytes_read;
+        }
+    }
+    else if let Some(cc) = header.get("Connection") {
+        if cc != "close" {
+            return Err(HttpError::InvalidHeader);
+        }
+        
+        body = response[header_end_pos + 4..].to_vec();
+        
+        connection.read_to_end(&mut body).map_err(|e| HttpError::ReadFailure(e))?;
+    }
+    else {
+        return Err(HttpError::MissingHeader);
+    };
+    
+    Ok(Response { status_line, header, body: Some(body) })
+}
+
+fn deserialize_top(top_bytes: &[u8]) -> Result<(StatusLine, HashMap<String, String>), HttpError> {
+    let top_bytes = str::from_utf8(top_bytes).map_err(|e| HttpError::InvalidPayload(e))?;
+    let mut top_lines = top_bytes.split("\r\n");
+    
+    let status_line = match top_lines.next() {
+        Some(sl) => {
+            let mut sh_iter = sl.split(' ');
+            let version = match sh_iter.next() {
+                Some(ver) => Version::from_str(ver)?,
+                None => return Err(HttpError::UnexpectedEOF)
+            };
+            let status = match sh_iter.next() {
+                Some(sc) => sc.parse().map_err(|e| HttpError::InvalidStatus(e))?,
+                None => return Err(HttpError::UnexpectedEOF)
+            };
+            
+            StatusLine { version, status }
+        }
+        None => return Err(HttpError::UnexpectedEOF)
+    };
+    
+    let mut headers: HashMap<String, String> = HashMap::new();
+    while let Some(line) = top_lines.next() {
+        if line.is_empty() { continue; }
+        match line.split_once(':') {
+            Some((key, val)) => _ = headers.insert(key.to_string(), val.trim().to_string()),
+            None => return Err(HttpError::UnexpectedEOF)
+        }
+    }
+    
+    Ok((status_line, headers))
 }
--- spyne-net/src/http/h11/structures.rs
+++ spyne-net/src/http/h11/structures.rs
@@ -1,15 +1,32 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, io::Error, num::ParseIntError, str::Utf8Error};
+
+pub enum HttpError {
+    TcpFailure(Error),
+    WriteFailure(Error),
+    TimeoutFailure(Error),
+    ReadFailure(Error),
+    ReadTimeout(Error),
+    InvalidPayload(Utf8Error),
+    InvalidStatus(ParseIntError),
+    InvalidContentLength(ParseIntError),
+    InvalidChunkSize(ParseIntError),
+    ChunkSizeLimitExceeded,
+    UnexpectedEOF,
+    InvalidVersion,
+    InvalidHeader,
+    MissingHeader
+}
 
 pub struct Request {
-    request_line: RequestLine,
-    header: HashMap<String, String>,
-    body: Option<Vec<u8>>
+    pub request_line: RequestLine,
+    pub header: HashMap<String, String>,
+    pub body: Option<Vec<u8>>
 }
 
 pub struct RequestLine {
-    method: Method,
-    path: String,
-    version: Version
+    pub method: Method,
+    pub path: String,
+    pub version: Version
 }
 
 pub enum Method {
@@ -24,19 +41,54 @@ pub enum Method {
     TRACE
 }
 
+impl Method {
+    pub(super) fn as_bytes(&self) -> &[u8] {
+        match self {
+            Method::GET => b"GET",
+            Method::POST => b"POST",
+            Method::PUT => b"PUT",
+            Method::DELETE => b"DELETE",
+            Method::PATCH => b"PATCH",
+            Method::HEAD => b"HEAD",
+            Method::OPTIONS => b"OPTIONS",
+            Method::CONNECT => b"CONNECT",
+            Method::TRACE => b"TRACE"
+        }
+    }
+}
+
 pub struct Response {
-    status_line: StatusLine,
-    header: HashMap<String, String>,
-    body: Option<Vec<u8>>
+    pub status_line: StatusLine,
+    pub header: HashMap<String, String>,
+    pub body: Option<Vec<u8>>
 }
 
 pub struct StatusLine {
-    version: Version,
-    status: u16
+    pub version: Version,
+    pub status: u16
 }
 
 pub enum Version {
     H11,
     H2,
     H3
+}
+
+impl Version {
+    pub(super) fn as_bytes(&self) -> &[u8] {
+        match self {
+            Version::H11 => b"HTTP/1.1",
+            Version::H2 => b"HTTP/2",
+            Version::H3 => b"HTTP/3"
+        }
+    }
+    
+    pub(super) fn from_str(s: &str) -> Result<Self, HttpError> {
+        match s {
+            "HTTP/1.1" => Ok(Version::H11),
+            "HTTP/2" => Ok(Version::H2),
+            "HTTP/3" => Ok(Version::H3),
+            _ => Err(HttpError::InvalidVersion)
+        }
+    }
 }
